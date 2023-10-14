f115e50b8ab558f50d60947765da4dba64c5ee8e
Auridebson Sousa <auridebson@vutus.com>
2023-10-14 15:08:07 -0300

    Update

--- README.rd
+++ README.rd
@@ -1,7 +1,13 @@
-1. Alterando o Estilo com Base em uma Condição
+**1. Alterando o Estilo com Base em uma Condição
 
 Crie um botão e um elemento <div> na página.
 Quando o botão for clicado, verifique se o tamanho da janela é menor que 500 pixels usando 
 window.innerWidth, e se for, altere a cor de fundo do <div> para vermelho; caso contrário, 
 mude para verde.
+
+**2. Controle de Visibilidade
+
+Crie três botões: "Mostrar", "Ocultar" e "Alternar" e um elemento <div> na página.
+Implemente funções de evento para cada botão que, respectivamente, mostrem, ocultem e alternem 
+a visibilidade do <div>.
 
--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 const btnMudaCor = document.querySelector("#btnMudaCor")
+const btnMostrar = document.querySelector("#btnMostrar")
+const btnOcultar = document.querySelector("#btnOcultar")
+const btnAlternar = document.querySelector("#btnAlternar")
+
 const containerWinwidthRes = document.querySelector("#container-winwidth-resultado")
+const containerButtonsManipulation = document.querySelector("#container-buttons-manipulation")
 
 const mudaCor500 = () => {
     let winWidth = window.innerWidth
@@ -10,8 +15,20 @@ const mudaCor500 = () => {
     }
 }
 
+const divChange = () => {
+    let ctrlDiv = 1
+
+    case
+
+    if (ctrlDiv === 1) {
+        containerButtonsManipulation.style.visibility = "visible"
+    }
+}
 
 
 
 
 btnMudaCor.addEventListener("click", mudaCor500)
+btnMostrar.addEventListener("click", divChange)
+btnOcultar.addEventListener("click", divChange)
+btnAlternar.addEventListener("click", divChange)
